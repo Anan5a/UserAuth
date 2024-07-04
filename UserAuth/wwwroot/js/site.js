9af1903f92a197af11fba811fd2795c00b6db347
Sayem Prodhan <ananta.ap69@gmail.com>
2024-07-04 12:34:32 +0600

    Added eyes to password field

--- UserAuth/wwwroot/js/site.js
+++ UserAuth/wwwroot/js/site.js
@@ -2,4 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
 //$(".toast").toast("show")
+
+document.addEventListener('click', function (event) {
+    if (event.target.matches(".eye-password")) {
+        const parentElement = event.target.parentElement;
+        const eyeIcon = parentElement.querySelector('.eye-password');
+        const passwordField = parentElement.querySelector('input.password');
+        const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
+        passwordField.setAttribute('type', type);
+        eyeIcon.classList.toggle('fa-eye');
+        eyeIcon.classList.toggle('fa-eye-slash');
+        eyeIcon.style.right = '10px';
+       
+    }
+});
